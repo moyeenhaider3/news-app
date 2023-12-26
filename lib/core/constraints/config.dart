4fd1f54e0955a136947567d0216081448e5f866c
Moyeen Haider <work.moyeen@gmail.com>
2023-12-26 20:31:04 +0530

    _

--- lib/core/constraints/config.dart
+++ lib/core/constraints/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   // static const apiKey = "d599203f3ccd4f68822563fed3918b28";
   // static const apiKey = "c55de70b17ab4e76a3466d7def6c80f7";
-  static const apiKey = "a8eb9ac7173d4f238f67ed278b28f16c";
-  // static const apiKey = "f83c70bae42247559b0e928770f16563";
+  // static const apiKey = "a8eb9ac7173d4f238f67ed278b28f16c";
+  static const apiKey = "f83c70bae42247559b0e928770f16563";
 }
